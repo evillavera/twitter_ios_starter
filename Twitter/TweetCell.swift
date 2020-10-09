b83bdfd470febfadf61d787e4d32dd54dc6bdcc4
Erik Villavera <villaveraerik@gmail.com>
2020-10-09 08:31:13 -0400

    Unit 3 work with bonuses done

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Erik Villavera on 10/8/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
